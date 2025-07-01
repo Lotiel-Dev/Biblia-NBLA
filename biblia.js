8b14728eb692791416db2b964667ce908fe68b90
Lotiel-Dev <karlos817gerceis@gmail.com>
2025-07-01 12:42:01 -0400

    Add Json para manejar todos los capitulos y versiculos y script para manejar el json en lugar de usar un texto plano

---
+++ biblia.js
@@ -0,0 +1,26 @@
+// biblia.js
+const biblia = {
+  "Genesis": {
+    1: [
+      "En el principio creó Dios los cielos y la tierra.",
+      "Y la tierra estaba desordenada y vacía, y las tinieblas estaban sobre la faz del abismo, y el Espíritu de Dios se movía sobre la faz de las aguas.",
+      "Y dijo Dios: Sea la luz; y fue la luz.",
+      "Y vio Dios que la luz era buena; y separó Dios la luz de las tinieblas.",
+      "Llamó Dios a la luz Día, y a las tinieblas llamó Noche. Y fue la tarde y la mañana un día.",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+    ],
+    2: [
+      "Fueron, pues, acabados los cielos y la tierra, y todo el ejército de ellos.",
+      "Y acabó Dios en el día séptimo la obra que hizo; y reposó el día séptimo de toda la obra que hizo.",
+      "Y bendijo Dios al día séptimo, y lo santificó; porque en él reposó de toda su obra que había creado y hecho."
+    ]
+  }
+};
